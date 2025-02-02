0c24ee7b8f1422e9ad36a4f2762cc37007f20aab
TalalMajeed <m.talal.majeed@gmail.com>
2025-02-02 13:08:43 +0500

    Added Screenshot Capturing Feature

---
+++ Sample/test.js
@@ -0,0 +1,4 @@
+//Hello to this Test Code Sample
+//This Page needs to be screenshotted
+//This is a Test File
+//Created by a NodeJS Developer
--- image.js
+++ image.js
@@ -1,42 +1,64 @@
 require("dotenv").config();
+const express = require("express");
 const puppeteer = require("puppeteer-extra");
 const StealthPlugin = require("puppeteer-extra-plugin-stealth");
 const { executablePath } = require("puppeteer");
-require("dotenv").config();
+const path = require("path");
+const http = require("http");
 
 puppeteer.use(StealthPlugin());
 
-const main = async () => {
-    browser = await puppeteer.launch({
-        headless: true,
-        executablePath: executablePath(),
-        args: [
-            "--incognito",
-            "--disable-features=IsolateOrigins,site-per-process",
-            "--disable-infobars",
-            "--no-sandbox",
-            "--disable-setuid-sandbox",
-            "--use-gl=angle",
-            "--use-angle=gl",
-            "--enable-unsafe-webgpu",
-            "--disable-setuid-sandbox",
-            "--use-fake-device-for-media-stream",
-            "--window-size=1280,900",
-        ],
-        defaultViewport: null,
-    });
+const app = express();
+const PORT = 3000;
 
-    const page = await browser.newPage();
-    await page.setViewport({
-        width: 1280,
-        height: 720,
-        deviceScaleFactor: 1,
-    });
+app.use(express.static(path.join(__dirname, "Frontend/dist")));
 
-    await page.goto("https://developer.mozilla.org/en-US/");
-    await page.waitForTimeout(5000);
-    await page.screenshot({ path: "example.png" });
-    await browser.close();
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "Frontend/dist/index.html"));
+});
+
+const server = http.createServer(app);
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+  startScraping();
+});
+
+const startScraping = async () => {
+  const browser = await puppeteer.launch({
+    headless: true,
+    executablePath: executablePath(),
+    args: [
+      "--incognito",
+      "--disable-features=IsolateOrigins,site-per-process",
+      "--disable-infobars",
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--use-gl=angle",
+      "--use-angle=gl",
+      "--enable-unsafe-webgpu",
+      "--disable-setuid-sandbox",
+      "--use-fake-device-for-media-stream",
+      "--window-size=1280,900",
+    ],
+    defaultViewport: null,
+  });
+
+  const page = await browser.newPage();
+  await page.setViewport({
+    width: 1280,
+    height: 720,
+    deviceScaleFactor: 1,
+  });
+
+  await page.goto(`http://localhost:${PORT}`);
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  await page.screenshot({ path: "screenshot.png" });
+  console.log("Screenshot saved.");
+
+  await browser.close();
+  console.log("Browser closed.");
+
+  server.close(() => {
+    console.log("Server shut down.");
+  });
 };
-
-main();
