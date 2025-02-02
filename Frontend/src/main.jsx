1f04b0c8098ba8597141b711ee51ca98ec6108ff
TalalMajeed <m.talal.majeed@gmail.com>
2025-02-02 13:02:31 +0500

    Created the Frontend Editor

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Editor } from "@monaco-editor/react";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <main>
+      <Editor
+        className="monaco"
+        height={"100svh"}
+        defaultLanguage="javascript"
+      />
+    </main>
+  </StrictMode>
+);
